fa333d38bb9dc08317951c6d1ac42a6ad0c5ea1f
Ray Feng <ryfprivate@outlook.com>
2021-11-05 03:04:15 +1100

    Added pre location pan

--- roadtrip_zoa/Assets/com.dotdothorse.roadtrip/Scripts/Monos/Scene/Gameplay/GameplayManager.cs
+++ roadtrip_zoa/Assets/com.dotdothorse.roadtrip/Scripts/Monos/Scene/Gameplay/GameplayManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:de7bca03a97144ce585086fcae01fb1c7512fa7985da823302187cc0c71f6198
-size 14184
+oid sha256:f86b0c2cee384813d389cf977c4f0469ffe4536b20a1e978da761986d28c01bd
+size 14436
--- roadtrip_zoa/Assets/com.dotdothorse.roadtrip/Scripts/Monos/Scene/MainMenu/UI/UIMainMenuNav.cs
+++ roadtrip_zoa/Assets/com.dotdothorse.roadtrip/Scripts/Monos/Scene/MainMenu/UI/UIMainMenuNav.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:64af71296569fe8f47a2d496efbbc5e8835ad20f778d8849cbf08470a2d15f4d
-size 4719
+oid sha256:09c24032cbe4c93bf95be7f7f7c9a0c6a20eb869e56dd3a6d712f96a65f24aba
+size 4725
