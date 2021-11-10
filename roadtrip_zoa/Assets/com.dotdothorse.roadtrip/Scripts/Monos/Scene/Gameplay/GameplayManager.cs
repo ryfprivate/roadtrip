fd02ac4e88934851ecc1191e294d1219988df3eb
Ray Feng <ryfprivate@outlook.com>
2021-11-11 08:52:18 +1100

    Added new tunnel and rush speed lines effects

--- roadtrip_zoa/Assets/com.dotdothorse.roadtrip/Scripts/Monos/Scene/Gameplay/GameplayManager.cs
+++ roadtrip_zoa/Assets/com.dotdothorse.roadtrip/Scripts/Monos/Scene/Gameplay/GameplayManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dbabdbc54313e0c7d4f8e8685d22a92ab5c783660c5aebed57413cce637e0480
+oid sha256:228446688254c1c427adf607d49f88a4f304e2ad2e5218fe1776551c66289a67
 size 16494
