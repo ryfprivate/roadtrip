fe99537a6e6a9cb97b62d521186665707f5023d9
Ray Feng <ryfprivate@outlook.com>
2021-11-13 02:22:58 +1100

    Added middle balkan plains level

--- roadtrip_zoa/Assets/com.dotdothorse.roadtrip/Scripts/Monos/Level/LevelSegment.cs
+++ roadtrip_zoa/Assets/com.dotdothorse.roadtrip/Scripts/Monos/Level/LevelSegment.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d491a8ee9a0709065f1ba4c6fcfdf25bdda70e6d600932819865f12521a2ba69
-size 5094
+oid sha256:4b4eefbd142f64bd6e6ffa07ab63a404196ac248bc662a62171dd137056a914d
+size 5086
--- roadtrip_zoa/Assets/com.dotdothorse.roadtrip/Scripts/Monos/Scene/Location/LevelGenerator.cs
+++ roadtrip_zoa/Assets/com.dotdothorse.roadtrip/Scripts/Monos/Scene/Location/LevelGenerator.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ca8ed26376be96ddacf088898d0ac1dd76169545d1f3628d64edbe19a3cb552
-size 7386
+oid sha256:b872c6c7f6a0e2aecbc576c569c03183ae5bdd9c7c2669e14f744a0eee92cb80
+size 7447
